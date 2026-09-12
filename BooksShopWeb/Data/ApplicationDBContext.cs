218e77ea8eba9dca06c578e7b7389b51bf9df4dc
gauti02 <159661749+gauti02@users.noreply.github.com>
2026-09-12 17:05:09 +0530

    add App Db context

---
+++ BooksShopWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksShopWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        // add DbSet<TEntity> properties here
+    }
+}
